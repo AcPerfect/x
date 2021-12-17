1d374ecfc395c5ab43a4eafaefe704908f27739b
Perfect Arif <86031416+AcPerfect@users.noreply.github.com>
2021-12-17 06:50:46 +0600

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,32 @@
+//---------------------------------------------
+// owl-carousel 
+//---------------------------------------------
+$(document).ready(function() {
+              $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                responsiveClass: true,
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: false
+                  },
+                  1000: {
+                    items: 3,
+                    nav: true,
+                    loop: false,
+                    margin: 10
+                  }
+                }
+              })
+			  $('.counterUp').counterUp({
+				delay: 10,
+				time: 2000
+			});
+			var mixer=mixitup('.mix-container');
+            })
+			
